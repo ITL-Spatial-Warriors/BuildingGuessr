26c0c71a09acb34a5dff6f10c2e0e4adf0028509
Alexander Melekhin <amelekhin96@gmail.com>
2025-10-17 22:53:17 +0300

    fix CORS problem

--- services/api/src/api/app.py
+++ services/api/src/api/app.py
@@ -26,8 +26,8 @@ app = FastAPI(title="BuildingGuessr API", version=__version__)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
