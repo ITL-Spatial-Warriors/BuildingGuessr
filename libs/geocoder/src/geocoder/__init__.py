8c685d457154383e0cf9323f93fb5b21e9b5c072
Georgiy2035 <soblin1@rambler.ru>
2025-10-18 17:29:11 +0300

    Add ruff formating

--- libs/geocoder/src/geocoder/__init__.py
+++ libs/geocoder/src/geocoder/__init__.py
@@ -1,2 +1,5 @@
-def hello() -> str:
-    return "Hello from geocoder!"
+"""
+Geocoder Library.
+
+This package provides a function that returns adress and building point of buildings at immage.
+"""
--- libs/preprocessing/src/preprocessing/__init__.py
+++ libs/preprocessing/src/preprocessing/__init__.py
@@ -1,2 +1,5 @@
-def hello() -> str:
-    return "Hello from preprocessing!"
+"""
+Preprocessing Library.
+
+This package provides a function that returns preprocessed image in tensor form to enter it into VPR model.
+"""
